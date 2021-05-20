11d279146d44f895f91237b023e12bcd879d56b4
Eric <eric@lugg.com>
2021-05-20 17:17:42 -0600

    initial commit of exercise with latest react native

---
+++ README.md
@@ -0,0 +1,34 @@
+# React Native Exercise
+
+This is a vanilla React Native Application containing a single screen with a placeholder view that will be replaced with a custom component.
+
+**IMPORTANT** - Please do not add any UI libraries to complete this exercise - no carousel libraries, no swipeable row libriaries, etc. If you'd like to import libraries like `moment` for dates or `lodash` to make working with arrays and objects easier feel free :)
+
+### What You'll Be Building
+
+Lugg customers want the ability to schedule a Lugg in advance. In order to accomplish this we have designed a time picker allowing the customer to select the day and time window for when they would like their movers to arrive. A customer can schedule a Lugg no more than 30 days in advance. You do not need to build the entire screen just the time picker component in the highlighted area in the right image.
+
+![image](https://user-images.githubusercontent.com/59875/119059991-9a4a7e80-b98e-11eb-86e7-2fecb8014556.png)
+
+#### Notes on UX
+
+Below is a screenshot of how the component should function. If you need any clarification please ask :)
+
+- Both days and time windows should be scrollable.
+- As you scroll the days, the time windows adjacent should update to that corresponding day in sync and vice versa when scrolling the time windows.
+- You can implement this custom component on the platform of your choosing (iOS or Android). If you are feeling ambitious please feel free to try both.
+
+![lugg-time-picker](https://user-images.githubusercontent.com/59875/119059528-b994dc00-b98d-11eb-923b-34e61f95a054.gif)
+
+### Setup
+
+```shell
+$ git clone git@github.com:luggit/react-native-exercise.git
+$ cd react-native-exercise
+$ yarn install
+$ npx react-native run-ios
+```
+
+After running the above commands the iOS simulator should open showing an application that looks like the image below.
+
+![image](https://user-images.githubusercontent.com/59875/119060146-e09fdd80-b98e-11eb-896c-9bc2116d3832.png)
---
+++ src/app.js
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  StatusBar,
+} from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+
+import { Color, Font } from './constants';
+
+const Button = ({ onPress, children, ...props }) => {
+  return (
+    <TouchableOpacity onPress={onPress} activeOpacity={0.9} {...props}>
+      <SafeAreaView
+        edges={['right', 'bottom', 'left']}
+        style={Styles.ButtonRoot}>
+        <View style={Styles.ButtonInner}>
+          <Text style={Styles.ButtonLabel}>{children}</Text>
+        </View>
+      </SafeAreaView>
+    </TouchableOpacity>
+  );
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <SafeAreaProvider>
+        <View style={Styles.Container}>
+          <StatusBar barStyle="light-content" />
+          <View style={Styles.Header}>
+            <Text style={Styles.HeaderText}>
+              Welcome to the React Native Lugg Exercise!
+            </Text>
+          </View>
+          <View style={Styles.Body}>
+            <Text>Your Component Here</Text>
+          </View>
+          <View style={Styles.Footer}>
+            <Button style={Styles.Button}>Next</Button>
+          </View>
+        </View>
+      </SafeAreaProvider>
+    );
+  }
+}
+
+const Styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    backgroundColor: Color.primary,
+  },
+  Header: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  Footer: {},
+  Body: {
+    height: 248,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  HeaderText: {
+    ...Font.regular,
+    color: '#fff',
+    textAlign: 'center',
+    maxWidth: 320,
+  },
+  ButtonRoot: {
+    backgroundColor: Color.action,
+  },
+  ButtonInner: {
+    height: 56,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  ButtonLabel: {
+    ...Font.medium,
+    color: Color.primary,
+    fontSize: 18,
+  },
+});
+
+export default App;
